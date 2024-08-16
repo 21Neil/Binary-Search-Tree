bee172335ea7a4948c7df5f067770b369c25dfdb
21Neil <88711171+21Neil@users.noreply.github.com>
2024-08-16 23:08:55 +0800

    Change test function name to insertNode

--- BST.js
+++ BST.js
@@ -37,12 +37,12 @@ function createTree(arr) {
   }
 
   function insert(data) {
-    test(root, data);
-    function test(node, data) {
+    insertNode(root, data);
+    function insertNode(node, data) {
       if (node === null) return createNode(data);
-      if (data > node.data) node.right = test(node.right, data);
-      if (data < node.data) node.left = test(node.left, data);
-      return node
+      if (data > node.data) node.right = insertNode(node.right, data);
+      if (data < node.data) node.left = insertNode(node.left, data);
+      return node;
     }
   }
 
@@ -69,4 +69,4 @@ const test = createTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 const test2 = createTree([1, 2, 3]);
 test2.insert(4);
 prettyPrint(test.root);
-console.log(test2.root)
+console.log(test2.root);
